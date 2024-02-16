fadfcae28c80c091aa72c99f659d59e22575f74a
boriskzd <boriskzd@users.noreply.github.com>
2024-02-16 18:20:51 +0100

    import minified svg icons for CRA-5

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,84 +1,84 @@
-import Rainy3 from '../assets/amcharts_weather_icons_1_0_0/animated/rainy-3.svg';
-import Rainy6 from '../assets/amcharts_weather_icons_1_0_0/animated/rainy-6.svg';
-import Sunny from '../assets/amcharts_weather_icons_1_0_0/animated/day.svg';
-import Cloudy1 from '../assets/amcharts_weather_icons_1_0_0/animated/cloudy-day-1.svg';
-import Cloudy2 from '../assets/amcharts_weather_icons_1_0_0/animated/cloudy-day-2.svg';
-import Cloudy3 from '../assets/amcharts_weather_icons_1_0_0/animated/cloudy-day-3.svg';
-import Snow from '../assets/amcharts_weather_icons_1_0_0/animated/snowy-3.svg';
-import Thunder from '../assets/amcharts_weather_icons_1_0_0/animated/thunder.svg';
-import CloudyAtmosphere from '../assets/amcharts_weather_icons_1_0_0/animated/cloudy.svg';
-import Weather from '../assets/amcharts_weather_icons_1_0_0/animated/weather.svg';
+import Rainy3 from "../assets/weather-icons-svg/rainy-3.svg";
+import Rainy6 from "../assets/weather-icons-svg/rainy-6.svg";
+import Sunny from "../assets/weather-icons-svg/day.svg";
+import Cloudy1 from "../assets/weather-icons-svg/cloudy-day-1.svg";
+import Cloudy2 from "../assets/weather-icons-svg/cloudy-day-2.svg";
+import Cloudy3 from "../assets/weather-icons-svg/cloudy-day-3.svg";
+import Snow from "../assets/weather-icons-svg/snowy-3.svg";
+import Thunder from "../assets/weather-icons-svg/thunder.svg";
+import CloudyAtmosphere from "../assets/weather-icons-svg/cloudy.svg";
+import Weather from "../assets/weather-icons-svg/weather.svg";
 // icons are from: https://www.amcharts.com/free-animated-svg-weather-icons/
 // weather conditions: https://openweathermap.org/weather-conditions
 
 export const findIcon = (typeOfWeather) => {
-  let icon = null;
+	let icon = null;
 
-  switch (typeOfWeather.main) {
-    case 'Clear':
-      icon = Sunny;
-      break;
-    case 'Thunderstorm':
-      icon = Thunder;
-      break;
-    case 'Drizzle':
-      icon = Rainy3;
-      break;
-    case 'Rain':
-      switch (typeOfWeather.id) {
-        case 500:
-        case 501:
-        case 502:
-        case 503:
-        case 504:
-          icon = Rainy3;
-          break;
-        case 511:
-          icon = Snow;
-          break;
-        case 520:
-        case 521:
-        case 522:
-        case 523:
-          icon = Rainy6;
-          break;
-        default:
-          icon = Weather;
-      }
-      break;
-    case 'Clouds':
-      switch (typeOfWeather.id) {
-        case 801:
-          icon = Cloudy1;
-          break;
-        case 802:
-          icon = Cloudy2;
-          break;
-        case 803:
-        case 804:
-          icon = Cloudy3;
-          break;
-        default:
-          icon = Weather;
-      }
-      break;
-    case 'Snow':
-      icon = Snow;
-      break;
-    case 'Mist':
-    case 'Smoke':
-    case 'Haze':
-    case 'Dust':
-    case 'Fog':
-    case 'Sand':
-    case 'Ash':
-    case 'Squall':
-    case 'Tornado':
-      icon = CloudyAtmosphere;
-      break;
-    default:
-      icon = Weather;
-  }
+	switch (typeOfWeather.main) {
+		case "Clear":
+			icon = Sunny;
+			break;
+		case "Thunderstorm":
+			icon = Thunder;
+			break;
+		case "Drizzle":
+			icon = Rainy3;
+			break;
+		case "Rain":
+			switch (typeOfWeather.id) {
+				case 500:
+				case 501:
+				case 502:
+				case 503:
+				case 504:
+					icon = Rainy3;
+					break;
+				case 511:
+					icon = Snow;
+					break;
+				case 520:
+				case 521:
+				case 522:
+				case 523:
+					icon = Rainy6;
+					break;
+				default:
+					icon = Weather;
+			}
+			break;
+		case "Clouds":
+			switch (typeOfWeather.id) {
+				case 801:
+					icon = Cloudy1;
+					break;
+				case 802:
+					icon = Cloudy2;
+					break;
+				case 803:
+				case 804:
+					icon = Cloudy3;
+					break;
+				default:
+					icon = Weather;
+			}
+			break;
+		case "Snow":
+			icon = Snow;
+			break;
+		case "Mist":
+		case "Smoke":
+		case "Haze":
+		case "Dust":
+		case "Fog":
+		case "Sand":
+		case "Ash":
+		case "Squall":
+		case "Tornado":
+			icon = CloudyAtmosphere;
+			break;
+		default:
+			icon = Weather;
+	}
 
-  return icon;
+	return icon;
 };
