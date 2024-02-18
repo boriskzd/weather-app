1e81c03d62d6f8c5f15485625380704dca741fed
boriskzd <boriskzd@users.noreply.github.com>
2024-02-18 19:53:39 +0100

    add explanation how to build in readme.md

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,60 +1,75 @@
 import {
-  Card,
-  CardContent,
-  Chip,
-  Divider,
-  Paper,
-  Typography,
-} from '@mui/material';
+	Card,
+	CardContent,
+	Chip,
+	Divider,
+	Paper,
+	Typography,
+} from "@mui/material";
 
-import SearchCity from './SearchCity';
-import CurrentWeather from './CurrentWeather';
-import WeatherChart from './WeatherChart';
-import NextWeek from './NextWeek';
+import SearchCity from "./SearchCity";
+import CurrentWeather from "./CurrentWeather";
+import WeatherChart from "./WeatherChart";
+import NextWeek from "./NextWeek";
 
-import { useGetWeatherByCityQuery } from '../store/weatherApi';
-import { useSelector } from 'react-redux';
+import { useGetWeatherByCityQuery } from "../store/weatherApi";
+import { useSelector } from "react-redux";
 
 export default function Weather() {
-  const position = useSelector((state) => state.weather); // coordinates of city
+	const position = useSelector((state) => state.weather); // coordinates of city
 
-  const { data, isLoading } = useGetWeatherByCityQuery(position);
+	const { data, isLoading } = useGetWeatherByCityQuery(position);
 
-  const loadingHtml = (
-    <div style={{ color: 'white', textAlign: 'center' }}>
-      <div>Loading...</div>
-    </div>
-  );
+	const loadingHtml = (
+		<div style={{ color: "white", textAlign: "center" }}>
+			<div>Loading...</div>
+		</div>
+	);
 
-  if (isLoading) return loadingHtml;
+	if (isLoading) return loadingHtml;
 
-  return (
-    <>
-      <Typography
-        variant='h4'
-        align='center'
-        sx={{ color: 'white', paddingTop: 2, paddingBottom: 2 }}
-      >
-        Weather App
-      </Typography>
+	const isLoadingSkeleton = (
+		<Card>
+			<CardContent>
+				<CurrentWeather data={data.current} />
 
-      <SearchCity />
+				<WeatherChart data={data.hourly} />
 
-      <Paper elevation={0}>
-        <Card>
-          <CardContent>
-            <CurrentWeather data={data.current} />
+				<Divider>
+					<Chip label="Next 7 days" size="small" />
+				</Divider>
 
-            <WeatherChart data={data.hourly} />
+				<NextWeek data={data} />
+			</CardContent>
+		</Card>
+	);
 
-            <Divider>
-              <Chip label='Next 7 days' size='small' />
-            </Divider>
+	return (
+		<>
+			<Typography
+				variant="h4"
+				align="center"
+				sx={{ color: "white", paddingTop: 2, paddingBottom: 2 }}
+			>
+				Weather App
+			</Typography>
 
-            <NextWeek data={data} />
-          </CardContent>
-        </Card>
-      </Paper>
-    </>
-  );
+			<SearchCity />
+			<Paper elevation={0}>
+				<Card>
+					<CardContent>
+						<CurrentWeather data={data.current} />
+
+						<WeatherChart data={data.hourly} />
+
+						<Divider>
+							<Chip label="Next 7 days" size="small" />
+						</Divider>
+
+						<NextWeek data={data} />
+					</CardContent>
+				</Card>
+			</Paper>
+		</>
+	);
 }
